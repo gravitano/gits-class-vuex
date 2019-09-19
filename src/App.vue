5264a2a83986752a9bab84fa43cf5b36c8fda6b1
Warsono <warsono16694@gmail.com>
2019-09-19 21:10:04 +0700

    hide menu & added title

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <h1>Vuex Shopping Cart</h1>
+    <!--    <div id="nav">-->
+    <!--      <router-link to="/">Home</router-link> |-->
+    <!--      <router-link to="/about">About</router-link>-->
+    <!--    </div>-->
     <router-view />
   </div>
 </template>
